89f1ce6f4fc82fb4d4429f6190f1f2e4d52ce718
inni-ast <inni_@tut.by>
2023-02-06 16:46:43 +0300

    feat: add start page btn

--- src/components/sections/main-section.ts
+++ src/components/sections/main-section.ts
@@ -1,29 +1,39 @@
 import Component from "../templates/component";
+import { createEl } from "../templates/functions";
 
 class MainSection extends Component {
-    constructor(tagName: string, clasName: string) {
-        super(tagName, clasName);
-    }
+  slider: HTMLElement;
+  constructor(tagName: string, className: string) {
+    super(tagName, className);
+    this.slider = document.createElement('div');
+    this.slider.classList.add('start__slider', 'slider');
+  }
+  makeHeader() {
+    const header = document.createElement('div');
 
+    header.className = 'start';
+    createEl('h1', 'start__header', header, 'The smart to-do app for busy people!');
+    createEl('button', 'btn start__btn', header, 'Sign up');
+    this.slider.innerHTML = this.makeSliderBtn();
+    header.append(this.slider);
+    return header;
+  }
+  makeSliderBtn() {
+    return `
+    <div class="slider__btns">
+      <input type="radio" id="btn-one" name="scales" checked>
+      <input type="radio" id="btn-two" name="scales">
+      <input type="radio" id="btn-three" name="scales">
+      <input type="radio" id="btn-four" name="scales">
+    </div>
+    `
+  }
+  render() {
+    const header = this.makeHeader();
 
-    testFunction() {
-        let test = document.createElement('h1');
-        test.className = 'test';
-        test.textContent = 'TEST MAIN SECTION';
-
-
-        return test
-    }
-
-
-    render() {
-        const testElemtn = this.testFunction();
-
-        this.container.append(testElemtn);
-
-
-        return this.container;
-    }
+    this.container.append(header);
+    return this.container;
+  }
 }
 
 export default MainSection;
