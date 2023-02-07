5bff9ca0bc1374ace0d5ed3c8165ccb790e04a36
inni-ast <inni_@tut.by>
2023-02-07 16:03:53 +0300

    feat: add slider

--- src/components/sections/main-section.ts
+++ src/components/sections/main-section.ts
@@ -1,7 +1,7 @@
 import Component from "../templates/component";
 import { createEl } from "../templates/functions";
 
-class MainSection extends Component {
+export class MainSection extends Component {
   slider: HTMLElement;
   constructor(tagName: string, className: string) {
     super(tagName, className);
@@ -14,17 +14,33 @@ class MainSection extends Component {
     header.className = 'start';
     createEl('h1', 'start__header', header, 'The smart to-do app for busy people!');
     createEl('button', 'btn start__btn', header, 'Sign up');
+
+    // const imageWrap = document.createElement("div");
+    // const image = document.createElement("div");
+    // imageWrap.classList.add('slider__image');
+    // image.classList.add('slider__img');
+
+    // imageWrap.append(image);
+
+    // this.slider.append(imageWrap, this.makeSliderBtn());
+
     this.slider.innerHTML = this.makeSliderBtn();
     header.append(this.slider);
     return header;
   }
+  static showSlideOne() {
+    console.log("slideOne")
+  }
   makeSliderBtn() {
     return `
+    <div class="slider__image">
+      <div class="slider__img slider__one" preload="lazy"></div>
+    </div>
     <div class="slider__btns">
-      <input type="radio" id="btn-one" name="scales" checked>
-      <input type="radio" id="btn-two" name="scales">
-      <input type="radio" id="btn-three" name="scales">
-      <input type="radio" id="btn-four" name="scales">
+      <input type="radio" class="slider__button" id="btn-one" name="scales" checked>
+      <input type="radio"  class="slider__button"id="btn-two" name="scales">
+      <input type="radio" class="slider__button" id="btn-three" name="scales">
+      <input type="radio"  class="slider__button"id="btn-four" name="scales">
     </div>
     `
   }
