ab1a2ba346220da91fe14b4cf2fce8f05ff50324
Angelica-zb <angelika.zaborovskaya@gmail.com>
2023-02-10 00:58:15 +0300

    layout app-page, add task start

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "./index.scss";
 import { listenSlider } from "./pages/listeners/slider";
+import { addTask } from "./pages/listeners/addtask";
 
 import App from './pages/app/app';
 
 const app = new App();
 app.start();
 listenSlider();
+addTask()
