3dc0faa3c41224cd668da59f4bc13297a24bd9f8
inni-ast <inni_@tut.by>
2023-02-09 14:30:00 +0300

    fix: change start-page styles

--- src/pages/app/app.ts
+++ src/pages/app/app.ts
@@ -4,54 +4,54 @@ import Page from '../../components/templates/page';
 import Header from '../../components/header/header';
 
 const enum PageIds {
-    MainPage = 'main-page',
-    ApplicationPage = 'application-page'
+  MainPage = 'main-page',
+  ApplicationPage = 'application-page'
 }
 
 class App {
-    private static container: HTMLElement = document.body;
-    private static defaultPageId: string = 'current-page';
-    private initialPage: MainPage;
-   // private header: Header;
+  private static container: HTMLElement = document.body;
+  private static defaultPageId: string = 'current-page';
+  private initialPage: MainPage;
+  // private header: Header;
 
-    static renderNewPage(idPage: string) {
-        const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
-        if (currentPageHTML) {
-            currentPageHTML.remove();
-        }
-
-        let page: Page | null = null;
-        if (idPage === PageIds.MainPage) {
-            page = new MainPage(idPage);
-        } else if (idPage === PageIds.ApplicationPage) {
-            page = new ApplicationPage(idPage);
-        }
-        if (page) {
-            const pageHTML = page.render();
-            pageHTML.id = App.defaultPageId;
-            App.container.append(pageHTML);
-        }
+  static renderNewPage(idPage: string) {
+    const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
+    if (currentPageHTML) {
+      currentPageHTML.remove();
     }
 
-    private enableRouteChange() {
-        window.addEventListener('hashchange', () => {
-            const hash = window.location.hash.slice(1);
-            App.renderNewPage(hash);
-
-        })
+    let page: Page | null = null;
+    if (idPage === PageIds.MainPage) {
+      page = new MainPage(idPage);
+    } else if (idPage === PageIds.ApplicationPage) {
+      page = new ApplicationPage(idPage);
     }
-
-    constructor() {
-        this.initialPage = new MainPage('main-page');
-       // this.header = new Header('header', 'header');
+    if (page) {
+      const pageHTML = page.render();
+      pageHTML.id = App.defaultPageId;
+      App.container.append(pageHTML);
     }
+  }
 
-    start() {
-      //  App.container.append(this.header.render());
-        App.renderNewPage('main-page');
-        this.enableRouteChange();
+  private enableRouteChange() {
+    window.addEventListener('hashchange', () => {
+      const hash = window.location.hash.slice(1);
+      App.renderNewPage(hash);
 
-    }
+    })
+  }
+
+  constructor() {
+    this.initialPage = new MainPage('main-page');
+    // this.header = new Header('header', 'header');
+  }
+
+  start() {
+    //  App.container.append(this.header.render());
+    App.renderNewPage('main-page');
+    this.enableRouteChange();
+
+  }
 }
 
 export default App;
--- src/pages/listeners/slider.ts
+++ src/pages/listeners/slider.ts
@@ -7,20 +7,20 @@ export const listenSlider = () => {
       if (target.id === 'btn-one') {
         el.classList.remove('slider__two', 'slider__three', 'slider__four');
         el.classList.add('slider__one');
-        document.body.style.background = '#4169E1';
+        document.body.style.background = '#0060BF';
       } else if (target.id === 'btn-two') {
         el.classList.remove('slider__one', 'slider__three', 'slider__four');
         el.classList.add('slider__two');
-        document.body.style.background = '#1E90FF';
+        document.body.style.background = '#3292F2';
       }
       else if (target.id === 'btn-three') {
         el.classList.remove('slider__one', 'slider__two', 'slider__four');
         el.classList.add('slider__three');
-        document.body.style.background = '#8A2BE2';
+        document.body.style.background = '#6A4CA6';
       } else if (target.id === 'btn-four') {
         el.classList.remove('slider__one', 'slider__two', 'slider__three');
         el.classList.add('slider__four');
-        document.body.style.background = '#32CD32';
+        document.body.style.background = '#5BB84D';
       }
     }
   })
